ea3739f45c6eb16874fa9b4e4b9d87ca3e5e8fad
bold881 <dff881@gmail.com>
2017-03-25 17:57:51 +0800

    algorithm DAO Impl

--- src/main/java/fulltextsearch/dao/TableContentDAOKeyTableImpl.java
+++ src/main/java/fulltextsearch/dao/TableContentDAOKeyTableImpl.java
@@ -1,5 +1,40 @@
 package fulltextsearch.dao;
 
-public class TableContentDAOKeyTableImpl implements TableContentDAO<E> {
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import fulltextsearch.appdaemon.AppConfig;
+
+public class TableContentDAOKeyTableImpl<E> implements TableContentDAO<E> {
 	
+	private final String algorithm = "AES";
+
+	@SuppressWarnings("unchecked")
+	public List<E> getTableContent() {
+		List<E> lstKeys = null;
+		
+		Session session = AppConfig.getSessionFactory().openSession();
+		Transaction tx = null;
+		try {
+		    tx = session.beginTransaction();
+		    @SuppressWarnings("rawtypes")
+			Query query = session.createQuery("FROM KeyTable K where K.algorithm = :algorithm");
+		    query.setParameter(":algorithm", algorithm);
+		    query.setMaxResults(AppConfig.getMaxResultCount());
+		    
+		    lstKeys = query.getResultList();
+		    tx.commit();
+		}
+		catch (Exception e) {
+		    if (tx!=null) tx.rollback();
+		    e.printStackTrace();
+		}
+		finally {
+		    session.close();
+		}
+		return lstKeys;
+	}
 }
