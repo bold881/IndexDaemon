31b971f418343229e85eeda4ce7d1066b190bab8
bold881 <dff881@gmail.com>
2017-03-28 19:21:51 +0800

    update

---
+++ src/main/java/fulltextsearch/dao/DBHelper.java
@@ -0,0 +1,62 @@
+package fulltextsearch.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import fulltextsearch.appdaemon.AppConfig;
+
+public class DBHelper {
+	
+	Connection connection = null;
+	
+	private boolean GetConnection() {
+		if(connection != null) {
+			return true;
+		}
+		
+		String url = AppConfig.getConnectionUrl() + ";databaseName=" + AppConfig.getDatabaseName();
+		try {
+			connection = DriverManager.getConnection(url,
+					AppConfig.getUserName(),
+					AppConfig.getPassword());
+			
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+	
+	private void CloseConnection() {
+		try {
+			if(connection != null && !connection.isClosed()) {
+				connection.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	protected ResultSet doQuery(String query) {
+		
+		ResultSet resultSet = null;
+		
+		if(GetConnection()) {
+			
+			Statement statement;
+			try {
+				statement = connection.createStatement();
+				resultSet = statement.executeQuery(query);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			CloseConnection();
+		}
+		
+		return resultSet;
+	}
+}
---
+++ src/main/java/fulltextsearch/dao/DBHelperKeyTable.java
@@ -0,0 +1,24 @@
+package fulltextsearch.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import fulltextsearch.appdaemon.AppConfig;
+
+public class DBHelperKeyTable extends DBHelper{
+	
+	public String getAESPrivateKey() {
+		
+		String ftpPrivateKey = AppConfig.getFtpPrivateKey();
+		if(ftpPrivateKey == null) {
+			String querySQL = "SELECT CKEYSTR FROM SYS_009 WHERE ALGORITHM = 'AES'";
+			ResultSet rs = doQuery(querySQL);
+			try {
+				ftpPrivateKey = rs.getString(1);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return ftpPrivateKey;
+	} 
+}
